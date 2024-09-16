f5a7966421db269433778f15ed20c5fd80e8e537
jakurych <jakub.brych@protonmail.com>
2024-09-16 23:38:30 +0200

    Added navigation to main screen from header

--- app/src/main/java/com/example/scoutquest/ui/components/Header.kt
+++ app/src/main/java/com/example/scoutquest/ui/components/Header.kt
@@ -1,11 +1,11 @@
 package com.example.scoutquest.ui.components
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -17,6 +17,9 @@ import com.example.scoutquest.R
 import com.example.scoutquest.ui.navigation.LocalNavigation
 import com.example.scoutquest.ui.navigation.Profile
 import com.example.scoutquest.ui.navigation.Settings
+
+// Define the route as a constant
+const val MainScreenRoute = "/mainscreen"
 
 @Composable
 fun Header() {
@@ -32,7 +35,7 @@ fun Header() {
         // Profile Icon Button
         IconButton(onClick = { navController.navigate(Profile) }) {
             Icon(
-                painter = painterResource(id = R.drawable.icon_user), // Zastąp właściwym zasobem ikony
+                painter = painterResource(id = R.drawable.icon_user),
                 contentDescription = "Profile",
                 tint = Color.White
             )
@@ -42,13 +45,14 @@ fun Header() {
         Text(
             text = "Scout Quest",
             fontSize = 32.sp,
-            color = Color.White
+            color = Color.White,
+            modifier = Modifier.clickable { navController.navigate(MainScreenRoute) }
         )
 
         // Settings Icon Button
         IconButton(onClick = { navController.navigate(Settings) }) {
             Icon(
-                painter = painterResource(id = R.drawable.icon_setting), // Zastąp właściwym zasobem ikony
+                painter = painterResource(id = R.drawable.icon_setting),
                 contentDescription = "Settings",
                 tint = Color.White
             )
