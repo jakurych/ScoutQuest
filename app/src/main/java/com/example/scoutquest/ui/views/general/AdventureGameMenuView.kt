97a9b7d91e9df9d454d64f1543c1fac0f2c942fd
jakurych <jakub.brych@protonmail.com>
2025-01-19 23:01:26 +0100

    Added Exit button to game map

--- app/src/main/java/com/example/scoutquest/ui/views/general/AdventureGameMenuView.kt
+++ app/src/main/java/com/example/scoutquest/ui/views/general/AdventureGameMenuView.kt
@@ -5,12 +5,14 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.airbnb.lottie.compose.*
 import com.example.scoutquest.ui.components.AnimatedButton
 import com.example.scoutquest.ui.components.Header
 import com.example.scoutquest.ui.navigation.*
 
+@Preview
 @Composable
 fun AdventureGameMenuView() {
     val navController = LocalNavigation.current
